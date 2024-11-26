569e31a3576228f0248cc8b2ac2fa389f3da2b9a
ise-e <amsmam1234@hanyang.ac.kr>
2024-11-27 00:47:46 +0900

    add test class

---
+++ src/main.java
@@ -0,0 +1,10 @@
+public class main {
+
+    public static void main(final String[] args) {
+        System.out.println(add(3,4));
+    }
+
+    public static int add(int a, int b){
+        return a + b;
+    }
+}
